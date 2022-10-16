351cf207da7ffaa883e957aff76946406a3fe759
Laicasaane <laicasaane@outlook.com>
2022-10-17 01:17:13 +0700

    Add ExampleSourceGenerator

---
+++ Packages/com.laicasaane.unity-source-generator-demo/Source~/ExampleSourceGenerator/ExampleSourceGenerator/ExampleSourceGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+
+namespace ExampleSourceGenerator
+{
+    [Generator]
+    public class ExampleGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+
+            var sourceBuilder = new StringBuilder(
+            @"
+            using System;
+            namespace ExampleSourceGenerated
+            {
+                public static class Example
+                {
+                    public static string GetTestText() 
+                    {
+                        return ""This is from source generator ");
+
+            sourceBuilder.Append(DateTime.Now.ToString());
+
+            sourceBuilder.Append(
+                @""";
+                    }
+    }
+}
+");
+
+            context.AddSource("ExampleSourceGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context) { }
+    }
+}
